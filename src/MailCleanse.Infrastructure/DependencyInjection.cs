bdd4617d007e2b8642b11c4f15c8a75600145653
Edgar Villegas <narnagas@gmail.com>
2026-09-21 17:16:22 -0500

    Add Yahoo IMAP scan support: src/MailCleanse.Infrastructure/DependencyInjection.cs

--- src/MailCleanse.Infrastructure/DependencyInjection.cs
+++ src/MailCleanse.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,22 @@
+using MailCleanse.Core.Abstractions;
+using MailCleanse.Infrastructure.Yahoo;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MailCleanse.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddMailCleanseInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddMailCleanseInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        // Yahoo/IMAP services will be registered here.
+        services
+            .AddOptions<YahooMailOptions>()
+            .Bind(configuration.GetSection(YahooMailOptions.SectionName));
+
+        services.AddScoped<IMailboxReader, YahooMailboxReader>();
+
         return services;
     }
 }
