7e0bfc9c23a7fa5f2ebaa857a5a32b170158f115
Edgar Villegas <narnagas@gmail.com>
2026-09-21 23:39:24 -0500

    Add Yahoo OAuth PKCE configuration

---
+++ src/MailCleanse.Infrastructure/Yahoo/YahooOAuthOptions.cs
@@ -0,0 +1,12 @@
+namespace MailCleanse.Infrastructure.Yahoo;
+
+public sealed class YahooOAuthOptions
+{
+    public const string SectionName = "Mail:Yahoo:OAuth";
+
+    public string ClientId { get; init; } = string.Empty;
+    public string RedirectUri { get; init; } = string.Empty;
+    public string AuthorizationEndpoint { get; init; } = "https://api.login.yahoo.com/oauth2/request_auth";
+    public string TokenEndpoint { get; init; } = "https://api.login.yahoo.com/oauth2/get_token";
+    public string Scope { get; init; } = "openid mail-r";
+}
